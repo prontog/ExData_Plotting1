875ba601d4e4d10b6885b71c7ac50f6a3c9febd2
Panos Rontogiannis <p_ronto@hotmail.com>
2014-05-09 18:05:53 +0300

    Improved loading time by skipping rows and reading only the 2880 rows for the two specific dates. Not very flexible but works.

--- plot1.R
+++ plot1.R
@@ -1,14 +1,20 @@
-# Read the whole thing.
-hpc <- read.csv("household_power_consumption.txt", sep=";")
-# Convert the Date column from factor to date.
-hpc$Date <-as.Date(hpc$Date, format="%d/%m/%Y")
-# Subset the observations from 2007-02-01 to 2007-02-01.
-hpc_part <- subset(hpc, Date >= "2007-02-01" & Date <= "2007-02-02")
-# Remove the huge object.
-rm(hpc)
-
-gap <- as.numeric(levels(hpc_part$Global_active_power)[hpc_part$Global_active_power])
+hpc_header <- read.csv("household_power_consumption.txt", 
+                       sep=";", 
+                       nrows=1, 
+                       header=TRUE)
+# Read the rows for the two dates of interest.
+hpc <- read.csv("household_power_consumption.txt", 
+                sep=";", 
+                skip=66637, 
+                nrows=2880,
+                col.names = colnames(hpc_header))
+# Remove header object. Not needed anymore.
+rm(hpc_header)
 
 png("plot1.png", width=480, height=480)
-hist(gap, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+hist(hpc$Global_active_power, 
+     col="red", 
+     xlab="Global Active Power (kilowatts)", 
+     main="Global Active Power")
 dev.off()
+
